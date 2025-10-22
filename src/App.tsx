00a2dfb01bfc89611dc9b244ca3d7789e757da51
Md Waqib Sk <rentusk.1970@gmail.com>
2025-10-22 20:42:14 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-lime-300'>
+        helo
+        </div>
+   </>
+  )
+}
+
+export default App
